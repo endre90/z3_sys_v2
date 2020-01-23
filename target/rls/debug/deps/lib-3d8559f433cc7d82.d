69ccc65e19eee1eac6f95b164e956fa9cb8e974c
endre90 <endre90@gmail.com>
2020-01-24 00:30:07 +0100

    remove license just in case...

---
+++ target/rls/debug/deps/lib-3d8559f433cc7d82.d
@@ -0,0 +1,5 @@
+/home/endre/z3_sys_v2/target/rls/debug/deps/lib-3d8559f433cc7d82.rmeta: tests/lib.rs
+
+/home/endre/z3_sys_v2/target/rls/debug/deps/lib-3d8559f433cc7d82.d: tests/lib.rs
+
+tests/lib.rs:
---
+++ target/rls/debug/deps/z3_sys-08430d863ddbfeae.d
@@ -0,0 +1,15 @@
+/home/endre/z3_sys_v2/target/rls/debug/deps/z3_sys-08430d863ddbfeae.rmeta: src/lib.rs src/generated/mod.rs src/generated/ast_kind.rs src/generated/ast_print_mode.rs src/generated/decl_kind.rs src/generated/error_code.rs src/generated/goal_prec.rs src/generated/param_kind.rs src/generated/parameter_kind.rs src/generated/sort_kind.rs src/generated/symbol_kind.rs
+
+/home/endre/z3_sys_v2/target/rls/debug/deps/z3_sys-08430d863ddbfeae.d: src/lib.rs src/generated/mod.rs src/generated/ast_kind.rs src/generated/ast_print_mode.rs src/generated/decl_kind.rs src/generated/error_code.rs src/generated/goal_prec.rs src/generated/param_kind.rs src/generated/parameter_kind.rs src/generated/sort_kind.rs src/generated/symbol_kind.rs
+
+src/lib.rs:
+src/generated/mod.rs:
+src/generated/ast_kind.rs:
+src/generated/ast_print_mode.rs:
+src/generated/decl_kind.rs:
+src/generated/error_code.rs:
+src/generated/goal_prec.rs:
+src/generated/param_kind.rs:
+src/generated/parameter_kind.rs:
+src/generated/sort_kind.rs:
+src/generated/symbol_kind.rs:
